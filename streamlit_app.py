7fe66d6399a0e935df159b66baeac7a99a71a8e9
reddykaswa <reddykaswa@gmail.com>
2025-03-26 07:01:19 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose up to 5 ingradinets:',my_dataframe)
 
 if ingredients_list:
-
     ingredients_string = '';
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen+' '
@@ -37,6 +36,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
